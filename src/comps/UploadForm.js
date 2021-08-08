7feeae307172b1a773d81506061f3ba19a64ae87
RAHUL KUMAR <rahulkumarkvbr@gmail.com>
2021-08-08 04:53:43 +0000

    first commit

---
+++ src/comps/UploadForm.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+
+const UploadForm = () =>{
+    const [file,setFile] = useState(null);
+    const [error,setError] = useState(null);
+
+
+    const handleChange =(e) =>{
+        let selected = e.target.files[0];
+        const types = ['image/jpeg', 'image/png']
+        if(selected && types.includes(selected.type)){
+            setFile(selected); 
+            setError('');
+        }
+        else{
+            setFile(null);
+            setError("Please select a valid file type.(png or jpeg)");
+        }
+    }
+
+    return(
+        <form>
+            <input type="file" accept="image/*" onChange={handleChange}/>
+            <div className="output">
+                {error && <div className="error">{error}</div>}
+                {file && <div>{file.name}</div>}
+            </div>
+        </form>
+    )
+}
+
+export default UploadForm;
---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import * as firebase from "firebse/app";
+import 'firebase/storage';
+import 'firebase/firestore';
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBCSI-RM_TDI6x0PyH_uA7EHfV02FOP4DU",
+    authDomain: "react-firegram-9c020.firebaseapp.com",
+    projectId: "react-firegram-9c020",
+    storageBucket: "react-firegram-9c020.appspot.com",
+    messagingSenderId: "334986505277",
+    appId: "1:334986505277:web:3e59df2a7f945eeff68e54"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  const projectStorage = firebase.storage();
+  const projectFirestore = firebase.firestore();
+
+  export {projectStorage,projectFirestore}
